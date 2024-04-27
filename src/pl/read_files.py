cdd067b54e51821ea961d1041b5aac2e93f2fb4b
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-04-27 19:24:38 +0100

    Refactoring JSOn reading

--- src/pl/read_files.py
+++ src/pl/read_files.py
@@ -1,36 +1,40 @@
 import json
 
-products_file_path = 'data/products.json'
-resources_file_path = 'data/resources.json'
+PRODUCTS_FILE_PATH = 'data/products.json'
+RESOURCES_FILE_PATH = 'data/resources.json'
+
+
+def load_json_data(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        # If the file does not exist or is empty/corrupted, start with an empty list
+        return []
+
+
+def save_json_data(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def add_data_item(file_path, new_item):
+    data = load_json_data(file_path)
+    data.append(new_item)
+    save_json_data(file_path, data)
 
 
 def load_products():
-    try:
-        with open(products_file_path, 'r') as file:
-            products = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        # If the file does not exist or is empty/corrupted, start with an empty list
-        products = []
-    return products
+    load_json_data(PRODUCTS_FILE_PATH)
 
 
 def load_resources():
-    with open(resources_file_path, 'r') as file:
-        resources = json.load(file)
-    return resources
-
-
-def save_products(products):
-    with open(products_file_path, 'w') as file:
-        json.dump(products, file, indent=4)
-
-
-def save_resources(resources):
-    with open(resources_file_path, 'w') as file:
-        json.dump(resources, file, indent=4)
+    load_json_data(RESOURCES_FILE_PATH)
 
 
 def add_product(new_product):
-    products = load_products()
-    products.append(new_product)
-    save_products(products)
+    add_data_item(PRODUCTS_FILE_PATH, new_product)
+
+
+def add_resource(new_resource):
+    add_data_item(RESOURCES_FILE_PATH, new_resource)
